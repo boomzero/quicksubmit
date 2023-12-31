7724a25977d062ea1b645bd8d6bbec94f0020d9e
boomzero <thomas_rainbowfish@icloud.com>
2023-12-31 19:52:11 +0800

    anyways

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const program = new Command();
 
 program
   .name("quickSubmit")
-  .version('v1.4.0')
+  .version('v1.4.1')
   .argument("[file]", "File to submit", "main.cpp")
   .requiredOption(
     "-p, --pid <number>",
