263e75a81c8846b400d3d9f1f23e8fb25a314c9c
boomzero <thomas_rainbowfish@icloud.com>
2024-01-01 14:36:14 +0800

    clean up config + other things

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
+/*
+ *     quickSubmit
+ *     Copyright (C) 2023-2024  boomzero
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { Command } from "commander";
 import { JSDOM } from "jsdom";
 import MD5 from "crypto-js/md5.js";
